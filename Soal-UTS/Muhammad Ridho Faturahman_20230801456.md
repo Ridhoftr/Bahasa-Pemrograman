426d759f7af22ca94f079f1612173332d5477e4b
root <root@iDoyy>
2024-05-18 09:33:45 +0700

    perubahan

---
+++ Soal-UTS/Muhammad Ridho Faturahman_20230801456.md
@@ -0,0 +1,15 @@
+# **SOAL 1**
+### Jelaskan sesuai dengan pemahaman anda apa itu Bahasa Pemrograman?
+# **JAWABAN**
+### Bahasa Pemrograman adalah sebuah intruksi standar untuk memerintah komputer agar menjalankan fungsi tertentu. Bahasa Pemrograman ini merupakan suatu himpunan dari aturan sintaksis dan semantik yang dipakai untuk mendefinisikan tata olah (*program*).
+# **SOAL 2**
+### Jelaskan berdasarkan pemahaman anda apa saja paradigma dalam Bahasa Pemrograman?
+# **JAWABAN**
+### Ada beberapa paradigma dalam Bahasa pemrograman, yaitu:
+1. ### **Impertatif** adalah paradigma yang berfokus pada menentukan langkah-langka yang diperlukan untuk mencapai tujuan.
+2. ### **Deklaratif** , deklaratif lebih berfokus pada mendefinisikan apa yang ingin dicapai, bukan cara-cara untuk mencapainya.
+3. ### **Fungsional** Paradigma ini berfokus pada fungsi sebagai unti utama komputasi. Dapat diterapkan pada data untuk menghasilkan output.
+4. ### **Obyek** Paradigma ini memodelkan program sebagai kumpulan objek yang memiliki atribut dan perilaku.
+# **SOAL 3**
+### Jelaskan type data yang anda ketahui dan berikan penjelasan terkait type data tersebut!
+# **JAWABAN**
---
+++ Soal-UTS/essay4.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+
+using namespace std;
+
+// Function prototypes
+void menu();
+int tambah(int, int);
+int kurang(int, int);
+int kali(int, int);
+int bagi(int, int);
+
+int main() {
+    menu();
+    int num1, num2;
+    int choice;
+    cout << "Masukan dua angka: ";
+    cin >> num1 >> num2;
+    cout << "1. Tambah\n2. Kurang\n3. Kali\n4. Bagi\nMasukan pilihan anda: ";
+    cin >> choice;
+    switch (choice) {
+        case 1:
+            cout << "Hasilnya adalah: " << tambah(num1, num2) << endl;
+            break;
+        case 2:
+            cout << "Hasilnya adalah: " << kurang(num1, num2) << endl;
+            break;
+        case 3:
+            cout << "Hasilnya adalah: " << kali(num1, num2) << endl;
+            break;
+        case 4:
+            if (num2 != 0)
+                cout << "Hasilnya adalah: " << bagi(num1, num2) << endl;
+            else
+                cout << "Error: Pembagian dengan nol tidak bisa!." << endl;
+            break;
+        default:
+            cout << "Pilihan tidak ada!" << endl;
+            break;
+    }
+    return 0;
+}
+
+void menu() {
+    cout << "Welcome to Simple Calculator!\n";
+}
+
+int tambah(int num1, int num2) {
+    return num1 + num2;
+}
+
+int kurang(int num1, int num2) {
+    return num1 - num2;
+}
+
+int kali(int num1, int num2) {
+    return num1 * num2;
+}
+
+int bagi(int num1, int num2) {
+    return num1 / num2;
+}
